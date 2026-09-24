917e69b2e6a7d7c98271dbb1f887e8e42432454d
BeatriciePanfil <panfil.beatricie@gmail.com>
2026-09-24 22:09:52 +0100

    Styled the app

--- src/Result.jsx
+++ src/Result.jsx
@@ -6,20 +6,23 @@ export default function Result(props) {
   if (props.results) {
     return (
       <div>
-        <h2>{props.results.word}</h2>
-
-        <div>
+        {" "}
+        <section>
+          <h2>{props.results.word}</h2>
+          <p>{props.results.phonetic}</p>
+        </section>
+        <section>
           {props.results.meanings.map(function (meanings, index) {
             return (
               <div key={index}>
-                <div className="part-of-speech">{meanings.partOfSpeech}</div>
+                <h3>{meanings.partOfSpeech}</h3>
                 <div className="definition">{meanings.definition}</div>
                 <div className="example">{meanings.example}</div>
                 <Synonyms synonyms={meanings.synonyms} />
               </div>
             );
           })}
-        </div>
+        </section>
       </div>
     );
   } else {
