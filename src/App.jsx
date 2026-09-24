f23d7a89f186785a515067d9346fb16299bdc016
BeatriciePanfil <panfil.beatricie@gmail.com>
2026-09-24 15:39:33 +0100

    Set up the project, added footer and main component

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Dictionary from "./Dictionary";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Dictionary />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/BeatriciePanfil"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Beatricie Panfil
+        </a>
+        , is{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
