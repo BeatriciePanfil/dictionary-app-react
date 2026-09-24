f9af1ee5375c8f48553758d92ae3b78d494b08e1
BeatriciePanfil <panfil.beatricie@gmail.com>
2026-09-24 22:09:06 +0100

    Added Api and created a component to display photos according to the keyword

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [result, setResult] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResult(response.data);
+  }
+
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
   }
   function search() {
     let apiKey = "bb3502a4t5567900odf5c4da2a6e140f";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imageApiUrl).then(handlePhotosResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -34,16 +43,28 @@ export default function Dictionary(props) {
       <main>
         <section>
           <form onSubmit={handleSubmit}>
-            <input
-              className="search-input"
-              type="search"
-              onChange={handleKeywordChange}
-              defaultValue={props.defaultKeyword}
-            />
-            <input className="submit" type="submit" value="Search" />
+            <div className="row">
+              <div className="col-9 pe-0">
+                <input
+                  className="search-input"
+                  type="search"
+                  onChange={handleKeywordChange}
+                  defaultValue={props.defaultKeyword}
+                />
+              </div>
+              <div className="col-3 ps-0">
+                <button type="submit" className="submit d-none d-md-inline">
+                  Submit
+                </button>
+                <button type="submit" className="submit d-inline d-md-none p-0">
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </div>
+            </div>
           </form>
         </section>
         <Result results={result} />
+        <Photos results={photos} />
       </main>
     );
   } else {
---
+++ src/Photos.jsx
@@ -0,0 +1,26 @@
+export default function Photos(props) {
+  console.log(props.results);
+  if (props.results) {
+    return (
+      <section>
+        <div className="row">
+          {props.results.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt={photo.photographer}
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
